d7d1b16d83eae4d46daaf799ff20916f5aac20b5
Louis Gandelin <lgandelin@web-access.fr>
2016-11-03 15:15:33 +0100

    Added no index meta tag

--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
     <title>@yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="icon" type="image/png" href="{{ asset('img/favicon.ico') }}" />
+    <!--<link rel="icon" type="image/png" href="{{ asset('img/favicon.ico') }}" />-->
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
 </head>
 <body>
