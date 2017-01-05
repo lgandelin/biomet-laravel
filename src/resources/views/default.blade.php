9ede4a314ba8eb70b7f138d7933e6418dcef2270
Louis Gandelin <lgandelin@web-access.fr>
2017-01-05 15:36:45 +0100

    Left column integration

--- src/resources/views/default.blade.php
+++ src/resources/views/default.blade.php
@@ -3,9 +3,11 @@
 @section('main-content')
 
     @include('biomet::includes.navigation')
+
     @include('biomet::includes.left_column')
 
-    <div class="container-fluid">
+    <div class="main-content container-fluid">
         @yield('page-content')
     </div>
+
 @endsection
--- src/resources/views/includes/left_column.blade.php
+++ src/resources/views/includes/left_column.blade.php
@@ -5,10 +5,12 @@
 
             @include('biomet::pages.facility.includes.menu')
 
+            <li class="separator"></li>
+
             @if (Auth::user() && Auth::user()->profile_id === Webaccess\BiometLaravel\Models\User::PROFILE_ID_AROL_ENERGY_ADMINISTRATOR)
-                <li @if (preg_match('/facilities/', $current_route)) class="active" @endif><a href="{{ route('facilities') }}"><i class="fa fa-fw fa-cogs"></i> Gestion des sites</a></li>
-                <li @if (preg_match('/clients/', $current_route)) class="active" @endif><a href="{{ route('clients') }}"><i class="fa fa-fw fa-briefcase"></i> Gestion des clients</a></li>
-                <li @if (preg_match('/users/', $current_route)) class="active" @endif><a href="{{ route('users') }}"><i class="fa fa-fw fa-users"></i> Gestion des utilisateurs</a></li>
+                <li class="admin-tab @if (preg_match('/facilities/', $current_route)) active @endif"><a href="{{ route('facilities') }}"><i class="fa fa-fw fa-cogs"></i> Gestion des sites</a></li>
+                <li class="admin-tab @if (preg_match('/clients/', $current_route)) active @endif"><a href="{{ route('clients') }}"><i class="fa fa-fw fa-briefcase"></i> Gestion des clients</a></li>
+                <li class="admin-tab @if (preg_match('/users/', $current_route)) active @endif"><a href="{{ route('users') }}"><i class="fa fa-fw fa-users"></i> Gestion des utilisateurs</a></li>
             @endif
 
             @if (Auth::user() && Auth::user()->profile_id === Webaccess\BiometLaravel\Models\User::PROFILE_ID_CLIENT_ADMINISTRATOR))
