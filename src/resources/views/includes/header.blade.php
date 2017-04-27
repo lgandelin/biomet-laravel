663ebdad21c46d2f1ee1bdb81a08cba1d5a0fdab
Louis Gandelin <lgandelin@web-access.fr>
2017-04-27 09:48:50 +0200

    Added favicon

--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="robots" content="noindex, nofollow">
     <title>@yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <!--<link rel="icon" type="image/png" href="{{ asset('img/favicon.ico') }}" />-->
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
 </head>
 <body>
