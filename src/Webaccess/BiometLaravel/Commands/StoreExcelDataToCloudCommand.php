8d134c460a655dc4113d95397420756395546aa2
Louis Gandelin <lgandelin@web-access.fr>
2017-05-22 17:38:10 +0200

    Added the command to store Excel files to OVH cloud

---
+++ src/Webaccess/BiometLaravel/Commands/StoreExcelDataToCloudCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Webaccess\BiometLaravel\Commands;
+
+use DateInterval;
+use DateTime;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Webaccess\BiometLaravel\Services\FacilityManager;
+use Webaccess\BiometLaravel\Services\OVHObjectStorage;
+
+class StoreExcelDataToCloudCommand extends Command
+{
+    protected $signature = 'biomet:store-excel-data-to-cloud {date} {facility_id?}';
+
+    protected $description = 'Stocke les fichiers de données brutes au format Excel sur le cloud d\'OVH';
+
+    public function handle()
+    {
+        date_default_timezone_set('Europe/Paris');
+        ini_set('memory_limit', -1);
+
+        $facilities = FacilityManager::getAll(false);
+        if ($this->argument('facility_id')) {
+            $facilities = array(FacilityManager::getByID($this->argument('facility_id')));
+        }
+
+        foreach ($facilities as $facility) {
+            $folder = env('DATA_FOLDER_PATH') . '/xls/' . $facility->id;
+
+            $yesterdayDate = DateTime::createFromFormat('Y-m-d', $this->argument('date'))->setTime(0, 0, 0)->sub(new DateInterval('P1D'));
+            $dayFolder = $folder . '/' . $yesterdayDate->format('Y/m/d');
+
+            $containerName = $facility->id;
+            $fileName = $dayFolder . '/data.xlsx';
+            $fileNameInContainer = 'data-' . $yesterdayDate->format('Y-m-d') . '.xlsx';
+
+            if (file_exists($fileName)) {
+                (new OVHObjectStorage())->uploadFileToContainer($containerName, $fileName, $fileNameInContainer);
+
+                $this->info('Fichier de données archivé sur le cloud avec succès pour le site ' . $facility->id . ' à la date du ' . $yesterdayDate->format('d/m/Y'));
+            } else {
+                $this->error('Data file not existing : ' . $fileName);
+            }
+        }
+    }
+}
